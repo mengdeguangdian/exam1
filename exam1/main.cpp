f82c5e2a953e82d3192b5d73f442ee9eb6dcd534
mengdeguangdian <liuhaitian28@gmail.com>
2021-04-07 02:13:03 -0700

    1

--- exam1/main.cpp
+++ exam1/main.cpp
@@ -9,6 +9,7 @@ InterruptIn down(A4);
 AnalogOut out(D7);
 AnalogOut filtered_out(D13);
 AnalogIn filtered_data(A5);
+//DigitalOut led2(LED2);
 
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
 
@@ -281,63 +282,73 @@ void mode_down()
 
 void mode_select()
 {
+    //led2 = !led2;
     if(selection_flag == 0)
     {        
         uLCD.text_width(2); 
         uLCD.text_height(2);
         uLCD.color(RED);
-        uLCD.locate(2,2);
-        uLCD.printf("\n  10Hz");
+        uLCD.locate(2,0);
+        uLCD.printf("\n  1");
     }
     else if(selection_flag == 1)
     {
         uLCD.text_width(2); 
         uLCD.text_height(2);
         uLCD.color(RED);
-        uLCD.locate(2,4);
-        uLCD.printf("\n  50Hz");
+        uLCD.locate(2,2);
+        uLCD.printf("\n  1/2");
     }
     else if(selection_flag == 2)
     {
         uLCD.text_width(2); 
         uLCD.text_height(2);
         uLCD.color(RED);
+        uLCD.locate(2,4);
+        uLCD.printf("\n  1/4");
+    }
+    else if(selection_flag == 3)
+    {
+        uLCD.text_width(2); 
+        uLCD.text_height(2);
+        uLCD.color(RED);
         uLCD.locate(2,6);
-        uLCD.printf("\n  100Hz");
+        uLCD.printf("\n  1/8");
     }
 
     float data;
-    if(selection_flag == 0) //10hz
+    if(selection_flag == 0) //1
     {
-        while(period_count<=(300))
+        while(1)
         {
-            loop_flag = loop_flag % 10;
-            for(int i=0;i<70;i++)
+            //loop_flag = loop_flag % 10;
+            for(int i=0;i<80;i++)
             {
-                data = (3.0f / 70.0f * i)/3.3f;
+                data = (3.0f / 80.0f * i)/3.3f;
                 out = data;
                 wait_us(500);
 
                 filtered_out = filtered_data; //adc
-                ADCdata1[loop_flag*100+i] = filtered_data;
+                //ADCdata1[loop_flag*100+i] = filtered_data;
                 //printf("%f\r\n", ADCdata1[loop_flag*100+i]);
 
                 wait_us(500);
             }
-            for(int j=0;j<30;j++)
+            ThisThread::sleep_for(80ms);
+            for(int j=0;j<80;j++)
             {
-                data = (3.0f - 3.0f / 30.0f * j)/3.3f;
+                data = (3.0f - 3.0f / 80.0f * j)/3.3f;
                 out = data;
                 wait_us(500);
 
                 filtered_out = filtered_data; //adc
-                ADCdata1[loop_flag*100+j+70] = filtered_data;
+                //ADCdata1[loop_flag*100+j+70] = filtered_data;
                 //printf("%f\r\n", ADCdata1[loop_flag*100+j+70]);
 
                 wait_us(500);
             } 
-            loop_flag++;
-            period_count++;
+            //loop_flag++;
+            //period_count++;
         }
     }
     else if(selection_flag == 1) //50hz
@@ -421,11 +432,24 @@ void mode_select()
     }
 }
 
+void print_data()
+{
+    for(int i=0; i<1000; i++)
+    {
+        printf("%f\r\n",(float)filtered_out);
+        ThisThread::sleep_for(10ms);
+    }
+}
 int main()
 {
     normal_mode();
     t.start(callback(&queue, &EventQueue::dispatch_forever));
-
+    //while(true)
+    //{
+    //    ThisThread::sleep_for(20s);
+    //queue.call(print_data);
+    //queue.call_(print_data);
+    //}
     /*while(true)
     {
         //printf("%f %f %f\n",up*1.0f, select_button*1.0f, down*1.0f);
@@ -444,5 +468,7 @@ int main()
     }*/
     up.rise(queue.event(mode_up));
     select_button.rise(queue.event(mode_select));
+    //select_button.fall(&print_data);
     down.rise(queue.event(mode_down));
+    //queue.dispatch();
 }
